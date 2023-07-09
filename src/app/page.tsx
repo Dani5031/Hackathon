7bb9cbe8e69b71adf45a40a2cc55012a7f72a5a9
Dani5031 <96937107+Dani5031@users.noreply.github.com>
2023-07-09 15:05:11 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import { Button } from "@/components/ui/button"
+
+
+export default function Home() {
+  return (
+    <main>
+    <button>
+      Click me
+    </button>
+    </main>
+  )
+}
